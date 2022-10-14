da6097d41e4c8f86987d531d66edd3b950744e09
Jatin Dhameliya <jatin.scalelot@gmail.com>
2022-10-14 16:38:53 +0530

    update notification image upload api

--- routes/apis/v1/notification.js
+++ routes/apis/v1/notification.js
@@ -4,8 +4,12 @@ const mongoConnection = require('../../../utilities/connections');
 const responseManager = require('../../../utilities/response.manager');
 const notificationModel = require('../../../models/notifications.model');
 const businessModel = require('../../../models/business.model');
+const usersModel = require('../../../models/users.model');
 const helper = require('../../../utilities/helper');
 const constants = require('../../../utilities/constants');
+const multerFn = require('../../../utilities/multer.functions');
+const AwsCloud = require('../../../utilities/aws');
+const allowedContentTypes = require("../../../utilities/content-types");
 const joiValidator = require('../../../models/validators/notificationvalidator');
 let mongoose = require('mongoose');
 router.post('/create', helper.authenticateToken, async (req, res) => {
@@ -99,6 +103,40 @@ router.get('/single', helper.authenticateToken, async (req, res) => {
         }
     } else {
         return responseManager.badrequest({ message: 'Invalid token to get notification, please try again' }, res);
+    }
+});
+router.post('/setnotificationbanner', helper.authenticateToken, async (req, res) => {
+    if (req.token.userid && mongoose.Types.ObjectId.isValid(req.token.userid)) {
+        let primary = mongoConnection.useDb(constants.DEFAULT_DB);
+        let userdata = await primary.model(constants.MODELS.users, usersModel).findById(req.token.userid).lean();
+        if (userdata) {
+            if (req.file) {
+                if (allowedContentTypes.imagearray.includes(req.file.mimetype)) {
+                    let filesizeinMb = parseFloat(parseFloat(req.file.size) / 1000000);
+                    if (filesizeinMb <= 10) {
+                        AwsCloud.saveToS3(req.file.buffer, userdata._id.toString(), req.file.mimetype, 'notification').then((result) => {
+                            var obj = {
+                                s3_url: process.env.AWS_BUCKET_URI,
+                                Key: result.data.Key
+                            };
+                            return responseManager.onSuccess('file added successfully...', obj, res);
+                        }).catch((err) => {
+                            return responseManager.onError(err, res);
+                        });
+                    } else {
+                        return responseManager.badrequest({ message: 'Images files must be less than 10 mb to upload, please try again' }, res);
+                    }
+                } else {
+                    return responseManager.badrequest({ message: 'Invalid image file formate for notification banner image, please try again' }, res);
+                }
+            } else {
+                return responseManager.badrequest({ message: 'Invalid image file please upload valid file, and try again' }, res);
+            }
+        } else {
+            return responseManager.badrequest({ message: 'Invalid user to upload product image, please try again' }, res);
+        }
+    } else {
+        return responseManager.badrequest({ message: 'Invalid token to upload product image, please try again' }, res);
     }
 });
 router.delete('/remove', helper.authenticateToken, async (req, res) => {
