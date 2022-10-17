f5fdd478ecf19f9ee111769f3bb4b50d9027cb9e
Jatin Dhameliya <jatin.scalelot@gmail.com>
2022-10-17 18:38:56 +0530

    updated chat apis

--- models/chats.model.js
+++ models/chats.model.js
@@ -16,6 +16,38 @@ let schema = new mongoose.Schema({
     contentType : {
         type: String,
 		require: true
+    },
+    content : {
+        text : {
+            message : {
+                type: String,
+                default: ''
+            }
+        },
+        media : {
+            path : {
+                type: String,
+                default: ''
+            },
+            type : {
+                type: String,
+                default: ''
+            },
+            mime : {
+                type: String,
+                default: ''
+            },
+            name : {
+                type: String,
+                default: ''
+            }
+        },
+        product : {
+            productid : {
+                type: mongoose.Types.ObjectId,
+                default: null
+            }
+        }
     }
 }, { timestamps: true, strict: false, autoIndex: true });
 schema.plugin(mongoosePaginate);
